45d5dde51a2660d4087c14662008c20cbde77a87
Lakshay Maini <62194911+lakshh07@users.noreply.github.com>
2021-02-23 18:43:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+var i = 0;
+var txt = `
+
+
+
+    Hey Harshita, 
+    Do you know the difference between 'But I love You' and 'I Love You' ? 
+    First one gives you an unconditional love and the second one drives you towards lots of condition. My love for you is that says no matter what happens, I will always be with you...💗
+    I think soo much and i made this after all i am programmer 😜 It took fucking so many lines of code to make this haha 😂, 
+    I propose you to be my lifetime without any condition I promise you that you have find no complaints in this relation. I want to be your true love and you can have me always by your side..
+    
+    I LOVE YOU!! 💝`
+var speed = 120;
+
+function typeWriter() {
+    if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+    }
+}
+
+document.querySelector(".play").addEventListener('click' , hello);
+function hello(){
+    document.querySelector('.start').classList.add("fadeOut");
+    document.querySelector(".play").classList.add("fadeOut")
+    typeWriter()
+};
+document.addEventListener('click', musicPlay);
+function musicPlay() {
+    document.getElementById('playAudio').play();
+    document.removeEventListener('click', musicPlay);
+}
