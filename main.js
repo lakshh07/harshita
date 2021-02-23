f4d35d8c4269039c446e2766fa37ea116bfef604
Lakshay Maini <62194911+lakshh07@users.noreply.github.com>
2021-02-23 19:50:34 +0530

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ var txt = `
     Do you know the difference between 'But I love You' and 'I Love You' ? 
     First one gives you an unconditional love and the second one drives you towards lots of condition. My love for you is that says no matter what happens, I will always be with you...💗
     I think soo much and i made this after all i am programmer 😜 It took fucking so many lines of code to make this haha 😂, 
-    I propose you to be my lifetime without any condition I promise you that you have find no complaints in this relation. I want to be your true love and you can have me always by your side..
+    I propose you to be my lifetime without any condition I promise you that you will find no complaints in this relation. I want to be your true love and you can have me always by your side..
     
     I LOVE YOU!! 💝`
 var speed = 120;
